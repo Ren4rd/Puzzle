a23223c890bd93292fbc2b1fec6111105844e1cb
bezrati1u <bezrati1u@localhost>
2014-02-22 11:58:25 +0100

    fin

--- src/NineSquarePuzzle.java
+++ src/NineSquarePuzzle.java
@@ -1,10 +1,28 @@
+import java.io.FileNotFoundException;
+
+import puzzle.Pool;
+
 
 public class NineSquarePuzzle {
 	
-    public  pool;
+    public Pool pool;
 	
+    
+    
+	public NineSquarePuzzle(String fileName) {
+		try {
+			this.pool = new Pool(fileName);
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+			System.out.println("Attention le fichier " + fileName + " n'existe pas !");
+		}
+	}
+
+
+
 	public boolean instParf(){
-	return (pool.toutParfait()){
+		
+	   return (this.pool.toutParfait());
 		
 	}
 
